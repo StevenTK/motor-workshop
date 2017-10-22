7e86f1990d2266bafd6997a59fc5f43b0e7fd30d
StevenTK <stk8m@virginia.edu>
2017-10-22 16:14:43 -0400

    Updated commenting on Arduino code

--- ArduinoCode/AdafruitDRV8871_SerialMotorControl/AdafruitDRV8871_SerialMotorControl.ino
+++ ArduinoCode/AdafruitDRV8871_SerialMotorControl/AdafruitDRV8871_SerialMotorControl.ino
@@ -1,43 +1,44 @@
-// Basic sketch for trying out the Adafruit DRV8871 Breakout
+// Oberlin DC Motor Control Workshop 10/27/2017
+// Steven Kemper, stevenTkemper@gmail.com
+// Control the Adafruit DRV8871 Breakout via Max MSP
+// Adapted from Arduino Serial Call/Response example code
 
-#define MOTOR_IN1 9
-#define MOTOR_IN2 10
+#define MOTOR_IN1 9 // Define pin 9 as "MOTOR_IN1"
+#define MOTOR_IN2 10 // Define pin 10 as "MOTOR_IN2"
+//
+// In this code, MOTOR_IN2 controls "fowrward" rotation
+// and MOTOR_IN1 controls "backward" rotation (DC mode)
+// MOTOR_IN1 controls solenoid (solenoid mode)
 
-String inData;
-int motor1Val = 0;
-int motor2Val = 0;
-int solenoidVal = 0;
+String inData; // variable to store incoming serial data
+int motor1Val = 0; //initalize value for global variable motor1Val
+int motor2Val = 0;//initalize value for global variable motor2Val
+int solenoidVal = 0;//initalize value for global variable solenoidVal
 
 void setup() {
-  Serial.begin(19200);
+  Serial.begin(19200); //Set serial communication speed
 
   Serial.println("Serial Motor Control");
   
-  pinMode(MOTOR_IN1, OUTPUT);
-  pinMode(MOTOR_IN2, OUTPUT);
+  pinMode(MOTOR_IN1, OUTPUT); // Set MOTOR_IN1 (pin 9) as output pin
+  pinMode(MOTOR_IN2, OUTPUT); // Set MOTOR_IN2 (pin 10) as output pin
 
 } 
 
 
-  
-
 void loop() {
 
-if (Serial.available() > 0) {
-    char recieved = Serial.read(); 
-    //delay(3); //give time for serial to read data (necessary for Xbees)
+if (Serial.available() > 0) { //If there is serial data
+    char recieved = Serial.read(); //read data from serial port
     inData += recieved; //Build a string from the incoming serial data
         // Process message when new line character is recieved
-     if (recieved == '\n') //If we get a newline char ASCII-10
+     if (recieved == '\n') //If we get a newline char ASCII-10--parse data when we get "\n"
      {
 //            Serial.print("Arduino Received: ");
 //            Serial.println(inData);
 
-            // You can put some if and else here to process the message juste like that:
+            // You can put some if and else here to process the message just like that:
 
-//            if(inData == "+++\n"){ // DON'T forget to add "\n" at the end of the string.
-//              Serial.println("OK. Press h for help.");
-//            }
 //            
          //Ignore the 'C's being sent from Max to trigger Serial.available
          if (inData.charAt(0) == 'C') { 
@@ -47,43 +48,36 @@ if (Serial.available() > 0) {
          }
          
           // f for DC motor forward
-           if(inData.charAt(0) == 'f') {
+           if(inData.charAt(0) == 'f') { //If the incoming message begins with an "f"
                // Serial.println("we got a f");
                 if (inData.length() > 1) {
                   String mySubstring;
-                  mySubstring = inData.substring(1);
-                  motor1Val = mySubstring.toInt();
-                  analogWrite(MOTOR_IN1, motor1Val);
-//                  if(motor1Val >255)
-//                  motor1Val = 255;
-//                  if(motor1Val <0)
-//                  motor1Val = 0;
-
+                  mySubstring = inData.substring(1); //read the next byte of data
+                  motor1Val = mySubstring.toInt(); //store that data as an integer
+                  analogWrite(MOTOR_IN1, motor1Val); //write PWM output to pin 9
                 }
              }  
              // r for DC motor reverse
-           if(inData.charAt(0) == 'r') {
+           if(inData.charAt(0) == 'r') { //If the incoming message begins with an "r"
                // Serial.println("we got a r");
                 if (inData.length() > 1) {
                   String mySubstring;
                   mySubstring = inData.substring(1);
                   motor2Val = mySubstring.toInt();
-                  analogWrite(MOTOR_IN2, motor2Val);
-//                  if(motor1Val >255)
-//                  motor1Val = 255;
-//                  if(motor1Val <0)
-//                  motor1Val = 0;
+                  analogWrite(MOTOR_IN2, motor2Val); //write PWM output to pin 10
 
                 }
              }  
              // s for solenoid
-           if(inData.charAt(0) == 's') {
+           if(inData.charAt(0) == 's') { //If the incoming message begins with an "s"
                // Serial.println("we got a s");
                 if (inData.length() > 1) {
                   String mySubstring;
                   mySubstring = inData.substring(1);
                   solenoidVal = mySubstring.toInt();
-                  analogWrite(MOTOR_IN1, solenoidVal);
+                  analogWrite(MOTOR_IN1, solenoidVal); //write PWM output to pin 9
+                    //Note, the solenoid control code is exactly the same as motor1Val code above
+                    //but allows separate controls from Max that are geared towards Solenoid control
 
                 }
              }  
